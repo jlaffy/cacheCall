f61ac1e4784d55c26947e72954a62e8ce4d375e6
Julie Laffy <julie.laffy@weizmann.ac.il>
2018-05-25 18:30:24 +0300

    run roxygen2md::roxygen2md on man/cacheCall.Rd

--- R/cacheCall.R
+++ R/cacheCall.R
@@ -24,9 +24,9 @@ string <- function(args, argNames=FALSE, sep=":", collapse="_") {
 #' @param fnName the name of function that is a step in the main function
 #' @param args a list of arguments to the main function
 #' @param cachePath a character string providing path to the Cache directory.
-#' @param sep passed to \code{string}. a character to separate arguments and their names. Defaults to ":".
-#' @param collapse passed to \code{string}. a character to separate arguments(+names) from one another. Defaults to "__".
-#' @param argNames passed to \code{string}. Boolean indicating whether argument names should be included in the final output.
+#' @param sep passed to `string`. a character to separate arguments and their names. Defaults to ":".
+#' @param collapse passed to `string`. a character to separate arguments(+names) from one another. Defaults to "__".
+#' @param argNames passed to `string`. Boolean indicating whether argument names should be included in the final output.
 #'
 #' @return character string that is the generated filename. It includes the main function and step function names, and the arguments.
 #' @export
@@ -47,16 +47,16 @@ makeFilename <- function(pipeName, fnName, args, cachePath, sep=":", collapse="_
 
 #' Call to Cache
 #'
-#' This function acts as a wrapper, such that for every real function call, enclosing it with \code{callCache()} first checks whether the output of the function call exists in \code{cachePath}.
-#' If the file does not exist, it executes the function call and saves the output to \code{cachePath}. If the file does exist, it reads the file.
+#' This function acts as a wrapper, such that for every real function call, enclosing it with `callCache()` first checks whether the output of the function call exists in `cachePath`.
+#' If the file does not exist, it executes the function call and saves the output to `cachePath`. If the file does exist, it reads the file.
 #' The filename is generated here and the file naming convention is determined by the function name(s) and arguments provided.
 #' @param fnName the name of the function (any function) being called.
 #' @param args a list of arguments that will define the filename.
-#' @param pipeName Can be the name of the parent function or the name of the pipeline if there is one. Can also simply be an ID for a series of functions and files that should be grouped together. Defaults to \code{NULL}.
+#' @param pipeName Can be the name of the parent function or the name of the pipeline if there is one. Can also simply be an ID for a series of functions and files that should be grouped together. Defaults to `NULL`.
 #' @param cachePath a character string providing path to the Cache directory.
 #' @param ... arguments that will be passed to fnName call if it is executed.
 #'
-#' @return the output of the function call, either from a read-in file or from an executed call. If the latter, the corresponding file will also be saved in \code{cachePath}.
+#' @return the output of the function call, either from a read-in file or from an executed call. If the latter, the corresponding file will also be saved in `cachePath`.
 #' @export
 #'
 #' @examples
